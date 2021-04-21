8763bfe62834dd9fbe21f98461121cc0e9ceeecc
hirokimaekawa <43659796+hirokimaekawa@users.noreply.github.com>
2021-04-21 21:39:00 +0900

    追加

--- Assets/Scripts/HammerManager.cs
+++ Assets/Scripts/HammerManager.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class HammerManager : MonoBehaviour
 {
+    //Animator animator;
+    public GameObject hammerObject;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("押された");
+            //animator.Play("Move@Hammer");
+            var item = Instantiate(hammerObject,transform.position,transform.rotation);
+            item.transform.SetParent(this.transform);
         }
     }
 }
