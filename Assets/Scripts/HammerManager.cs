324d4ce129638a7dd0daed0cffe59af759c4b13d
hirokimaekawa <43659796+hirokimaekawa@users.noreply.github.com>
2021-04-22 23:21:08 +0900

    SetTriggerを使用した

--- Assets/Scripts/HammerManager.cs
+++ Assets/Scripts/HammerManager.cs
@@ -7,21 +7,44 @@ public class HammerManager : MonoBehaviour
     //Animator animator;
     public GameObject hammerObject;
 
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        //animator = GetComponent<Animator>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var mousePosition = Input.mousePosition;
+        mousePosition.z = 10;
+        var pos = Camera.main.ScreenToWorldPoint(mousePosition);
+        hammerObject.transform.position = pos;
+
         if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("入力されてる");
+            animator.SetTrigger("Move0");
+        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+
+        //    var mousePosition = Input.mousePosition;
+        //    mousePosition.z = 10;
+        //    var pos = Camera.main.ScreenToWorldPoint(mousePosition);
+        //    var cube = Instantiate(hammerObject,transform);
+        //    cube.transform.position = pos;
+            
+        //}
+
+        /*if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("押された");
             //animator.Play("Move@Hammer");
             var item = Instantiate(hammerObject,transform.position,transform.rotation);
             item.transform.SetParent(this.transform);
-        }
+        }*/
     }
 }
